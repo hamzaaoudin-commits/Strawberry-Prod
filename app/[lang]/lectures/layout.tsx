28bb4e15db4739b65586093547444ae13cd8a0bd
hamzaaoudin-commits <hamzaa.oudin@gmail.com>
2026-08-05 20:04:52 +0200

    Delete app/[lang]/lectures directory

--- app/[lang]/lectures/layout.tsx
+++
@@ -1,4 +0,0 @@
-/** Layout hérité, neutralisé : la page ne fait plus que rediriger. */
-export default function LegacyLecturesLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
-}
