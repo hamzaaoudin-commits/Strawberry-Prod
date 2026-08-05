ae57c4d503ac7bf40dce48c6b7538d352f56def0
hamzaaoudin-commits <hamzaa.oudin@gmail.com>
2026-08-05 20:04:42 +0200

    Delete app/[lang]/offres directory

--- app/[lang]/offres/layout.tsx
+++
@@ -1,4 +0,0 @@
-/** Layout hérité, neutralisé : la page ne fait plus que rediriger. */
-export default function LegacyOffresLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
-}
